bb4250e036abc3c978e0b51b2a3d1671f28b57f3
Hoang Nguyen <hoangnguyen220396@gmail.com>
2021-02-17 11:38:19 +0700

    Hooks-UseEffect-fetchAPI

--- src/App.js
+++ src/App.js
@@ -1,39 +1,37 @@
-import logo from "./logo.svg";
-import Colorbox from "./components/ColorBox/Colorbox";
-import TodoList from "./components/TodoList/TodoList";
-import { useState } from "react";
-import TodoForm from "./components/TodoForm";
-const dataTodoList = [
-  { id: 1, title: "I love Clara Nguyen" },
-  { id: 2, title: "You love Clara Nguyen" },
-  { id: 3, title: "We love Clara Nguyen" },
-];
+import { useEffect, useState } from "react";
+import PostList from "./components/PostList";
+
 function App() {
-  const [todoList, setTodoList] = useState(dataTodoList);
-  function TodoClick(todo) {
-    console.log("App ne", todo);
-    const index = todoList.findIndex((x) => x.id === todo.id);
-    const newTodoList = [...todoList];
-    newTodoList.splice(index, 1);
-    setTodoList(newTodoList);
-  }
-  function handleTodoFormSubmit(formValues) {
-    console.log("Form submit ne: ", formValues);
-    const newTodo = {
-      id: todoList.length + 1,
-      ...formValues,
-    };
-    const newTodoList = [...todoList];
-    newTodoList.push(newTodo);
-    setTodoList(newTodoList);
-  }
+  const [postList, setPostList] = useState([{ id: 1, title: "hiih" }]);
+  useEffect(() => {
+    async function fetchPostList(params) {
+      //...
+      try {
+        const requestUrl =
+          "http://js-post-api.herokuapp.com/api/posts?_limit=12&_page=2";
+        const response = await fetch(requestUrl);
+        const responseJSON = await response.json();
+        console.log(responseJSON.data);
+        const { data } = responseJSON;
+
+        setPostList(data);
+      } catch (error) {
+        console.log("Failed to fetch post list: ", error.message);
+      }
+    }
+    console.log("Post List");
+
+    fetchPostList();
+  }, []);
+  useEffect(() => {
+    console.log("Todo List");
+  }, []);
   return (
     <div className="app">
       <div>Welcome to study React Hook</div>
-      <div>Roark Nguyen</div>
-      <Colorbox />
-      <TodoList todos={todoList} handleTodoClick={TodoClick} />
-      <TodoForm onSubmit={handleTodoFormSubmit} />
+      <div>React Hooks - Posts List</div>
+
+      <PostList posts={postList} />
     </div>
   );
 }
