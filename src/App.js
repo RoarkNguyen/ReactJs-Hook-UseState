246e9ed4b22b0e54a553d401256257c62337f7f5
Hoang Nguyen <hoangnguyen220396@gmail.com>
2021-02-17 14:26:46 +0700

    Hooks-useEffect-filter

--- src/App.js
+++ src/App.js
@@ -1,37 +1,54 @@
 import { useEffect, useState } from "react";
+import Pagination from "./components/Pagination";
+import queryString from "query-string";
 import PostList from "./components/PostList";
 
 function App() {
-  const [postList, setPostList] = useState([{ id: 1, title: "hiih" }]);
+  const [postList, setPostList] = useState([{ id: 1, title: "hihi" }]);
+  const [pagination, setPagination] = useState({
+    _page: 1,
+    _limit: 10,
+    _totalRows: 1,
+  });
+
+  const [filters, setFilters] = useState({
+    _limit: 10,
+    _page: 1,
+  });
+
   useEffect(() => {
     async function fetchPostList(params) {
       //...
       try {
-        const requestUrl =
-          "http://js-post-api.herokuapp.com/api/posts?_limit=12&_page=2";
+        const paramsString = queryString.stringify(filters);
+        const requestUrl = `http://js-post-api.herokuapp.com/api/posts?${paramsString}`;
         const response = await fetch(requestUrl);
         const responseJSON = await response.json();
-        console.log(responseJSON.data);
-        const { data } = responseJSON;
-
+        console.log({ responseJSON });
+        const { data, pagination } = responseJSON;
         setPostList(data);
+        setPagination(pagination);
       } catch (error) {
         console.log("Failed to fetch post list: ", error.message);
       }
     }
-    console.log("Post List");
 
     fetchPostList();
-  }, []);
-  useEffect(() => {
-    console.log("Todo List");
-  }, []);
+  }, [filters]);
+  function handlePageChange(newPage) {
+    console.log("New Page", newPage);
+    setFilters({
+      ...filters,
+      _page: newPage,
+    });
+  }
+
   return (
     <div className="app">
       <div>Welcome to study React Hook</div>
       <div>React Hooks - Posts List</div>
-
       <PostList posts={postList} />
+      <Pagination pagination={pagination} onPageChange={handlePageChange} />
     </div>
   );
 }
