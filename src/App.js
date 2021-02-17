4e467ab71416e8ec44cdb3c25945a1f822498b60
Hoang Nguyen <hoangnguyen220396@gmail.com>
2021-02-17 10:30:35 +0700

    UseState-Form-Basic

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import Colorbox from "./components/ColorBox/Colorbox";
 import TodoList from "./components/TodoList/TodoList";
 import { useState } from "react";
+import TodoForm from "./components/TodoForm";
 const dataTodoList = [
   { id: 1, title: "I love Clara Nguyen" },
   { id: 2, title: "You love Clara Nguyen" },
@@ -11,10 +12,20 @@ function App() {
   const [todoList, setTodoList] = useState(dataTodoList);
   function TodoClick(todo) {
     console.log("App ne", todo);
-    // const index = todoList.findIndex((x) => x.id === todo.id);
-    // const newTodoList = [...todoList];
-    // newTodoList.splice(index, 1);
-    // setTodoList(newTodoList);
+    const index = todoList.findIndex((x) => x.id === todo.id);
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  }
+  function handleTodoFormSubmit(formValues) {
+    console.log("Form submit ne: ", formValues);
+    const newTodo = {
+      id: todoList.length + 1,
+      ...formValues,
+    };
+    const newTodoList = [...todoList];
+    newTodoList.push(newTodo);
+    setTodoList(newTodoList);
   }
   return (
     <div className="app">
@@ -22,6 +33,7 @@ function App() {
       <div>Roark Nguyen</div>
       <Colorbox />
       <TodoList todos={todoList} handleTodoClick={TodoClick} />
+      <TodoForm onSubmit={handleTodoFormSubmit} />
     </div>
   );
 }
